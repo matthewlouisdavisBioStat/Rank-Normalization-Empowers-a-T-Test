121c72cb40692a7754b47038d11f4800acb8d8f7
matthewlouisdavisBioStat <67028278+matthewlouisdavisBioStat@users.noreply.github.com>
2020-11-06 10:18:42 -0600

    Update SimpleExample.R

--- SimpleExample.R
+++ SimpleExample.R
@@ -1,8 +1,8 @@
 
-# Simple Demonstration of Rank Normalization with a T-Test
+  # Simple demonstration of rank Normalization with a t-test using for loop
 
 ## Make up an OTU Table with Arbitrary Values
-otu_table <- matrix(rpois(100000,100),nrow= 1000)
+otu_table <- matrix(rnorm(100000),nrow= 1000)
 colnames(otu_table) <- sample(c("control sample","treatment sample"),
                               ncol(otu_table),replace= TRUE)
 control_indices <- which(colnames(otu_table) == "control sample")
@@ -22,3 +22,29 @@ for(i in 1:nrow(ranks)){
 
 ## Results for OTU 1
 results$OTU_1
+
+
+################################################################################
+
+    ## Increase the speed of a t-test with vectorization
+
+## Matt's function to conduct Welch's Two-Sample t-test across all rows at once
+fastT <- function(otu, ## otu_table, matrix with rows as taxa columns as samples
+                  indg1, ## treatment indices
+                  indg2){ ## control indices
+  l1 <- length(indg1)
+  l2 <- length(indg2)
+  S1 <- (rowSums((otu[,indg1] - rowMeans(otu[,indg1]))^2) / (l1 - 1))
+  S2 <- (rowSums((otu[,indg2] - rowMeans(otu[,indg2]))^2) / (l2 - 1))
+  sigmas <- sqrt ( S1/l1 + S2/l2 )
+  df <- sigmas^4  / ( ((l1^2 * (l1-1))^-1 *  S1^2) + ((l2^2 * (l2-1))^-1 *  S2^2) )
+  ## 
+  obsT <- (rowMeans(otu[,indg1]) - rowMeans(otu[,indg2])) / sigmas
+  ## 
+  pvals <- 2*(1-pt(abs(obsT), df = df))
+  ifelse(is.nan(pvals), 1, pvals)
+}
+
+## returns raw p-values
+results <- fastT(otu_table,treatment_indices,control_indices)
+head(results)
