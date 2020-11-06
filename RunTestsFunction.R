a9fcf68c90e8c431bfe78ff2047a5db60fbec050
matthewlouisdavisBioStat <67028278+matthewlouisdavisBioStat@users.noreply.github.com>
2020-11-06 11:03:37 -0600

    Add files via upload

---
+++ RunTestsFunction.R
@@ -0,0 +1,63 @@
+
+runTests <- function(physeq, truede = truede, degenes = degenes) {
+  returnList = list()
+  returnList = within(returnList, {
+    
+    ## all normalisations
+    # 
+     physeq <- normEdgeR(physeq = physeq, method = "TMM")
+     physeq <- normDESeq2(physeq = physeq)
+     physeq <- normCSS(physeq = physeq)
+     physeq <- normTSS(physeq = physeq)
+     physeq <- normNone(physeq)
+    sample_data <- sample_data(physeq)
+    sample_data$group <- sample_data$condition
+    sample_data(physeq) <- sample_data(sample_data)
+    cat("\nNormalisations\t")
+    
+    # When we rank, we want to normalize before trimming, not after 
+    # instead, we will FIRST rank, then only analyze our same trimmming scheme
+    source("C:/Users/Matthew/Documents/Courses/Kai/Final Results/RCode/rankTests (for running sim).R")
+    tTest_rank = tTest_rank
+    
+    cat("\nrankTest\t")
+    # 
+     tTest_TSS = applySimpleTests(physeq, test = "t-test", normFacts = "TSS")
+     tTest_TMM = applySimpleTests(physeq, test = "t-test", normFacts = "TMM")
+     tTest_CSS = applySimpleTests(physeq, test = "t-test", normFacts = "CSS")
+     tTest_None = applySimpleTests(physeq, test = "t-test", normFacts = "none")
+     wTest_TSS = applySimpleTests(physeq, test = "wilcox", normFacts = "TSS")
+     wTest_TMM = applySimpleTests(physeq, test = "wilcox", normFacts = "TMM")
+     wTest_CSS = try(applySimpleTests(physeq, test = "wilcox", normFacts = "CSS"), silent = TRUE)
+     wTest_None = applySimpleTests(physeq, test = "wilcox", normFacts = "none")
+     cat("\nSimple tests\t")
+    # 
+    # ## limma-voom robust version
+    voomTest_TMM = limmaVoomRobust(physeq, normFacts = "TMM")
+    cat("\nLimma-Voom robust tests\t")
+    # 
+    # ## we can extract both t-test and wilcox test from same run
+    aldexTTest_none <- aldexTTest(aldexTest(physeq))
+     cat("\nALDEx2\t")
+    # 
+    # ## edgeR robust version
+    edgeR_TMM = edgeRRobust(physeq, normFacts = "TMM")
+
+    # ## DESeq2 Default Pipeline
+     DESeq2_ratio = try(DESeq2(physeq, normFacts = "ratio"), silent = TRUE)
+     if(class(DESeq2_ratio) == "try-error"){
+       DESeq2_ratio = try(DESeq2(physeq, normFacts = "ratio"), silent = TRUE)
+     }
+    cat("\nNB DESeq2 tests\t")
+    # 
+    # ## metagenomeSeq Zero-Inflated Gaussian
+    mgsZig_CSS <- try(metagenomeSeqZIG(physeq, normFacts = "CSS"), silent = TRUE)
+    if(class(mgsZig_CSS) == "try-error"){
+      mgsZig_CSS <- try(metagenomeSeqZIG(physeq, normFacts = "CSS"), silent = TRUE)
+    }
+    rm(physeq)
+    rm(sample_data)
+  })
+  cat("Adj. WMW-PI tests\t")
+  return(returnList)
+}
---
+++ rankTests (for running sim).R
@@ -0,0 +1,71 @@
+
+#####################################################
+  ## Load Data
+start <- Sys.time()
+otu_table <- as(otu_table(physeq), "matrix")
+ncol <- ncol(otu_table)
+nrow <- nrow(otu_table)
+sample_data <- as(sample_data(physeq), "data.frame")
+indg1 <- which(sample_data$group == "grp1")
+indg2 <- which(sample_data$group == "grp2")
+
+  ## Remove rows containing only 0s, but make note of the 5% prevalant OTU
+  ## Recall: Other methodologies prefer 5% prevalance 
+minReads <- 1
+minPrev <- 0.05
+prevalence <- rowMeans(otu_table >= minReads)
+indOTUs2Keep <- (prevalence >= minPrev)
+nonzeroind <- indOTUs2Keep
+
+  ## Rank Normalization with a T-Test
+otu_table <- otu_table[rowSums(otu_table) > 0,] # 
+ranks <- apply(otu_table, 2, rank)
+l1 <- length(indg1)
+l2 <- length(indg2)
+S1 <- (rowSums((ranks[,indg1] - rowMeans(ranks[,indg1]))^2) / (l1 - 1))
+S2 <- (rowSums((ranks[,indg2] - rowMeans(ranks[,indg2]))^2) / (l2 - 1))
+sigmas <- sqrt ( S1/l1 + S2/l2 )
+df <- sigmas^4  / ( ((l1^2 * (l1-1))^-1 *  S1^2) + ((l2^2 * (l2-1))^-1 *  S2^2) )
+obsT <- (rowMeans(ranks[,indg1]) - rowMeans(ranks[,indg2])) / sigmas
+pvals <- 2*(1-pt(abs(obsT), df = df))
+pvals <- ifelse(is.nan(pvals), 1, pvals)
+names(pvals) <- rownames(ranks)
+
+# padj <- rep(1, length(pvals))
+pvals <- pvals[names(nonzeroind)]
+padj <- p.adjust(pvals, method = "BH")
+names(padj) <- names(pvals)
+
+mat <- cbind(c(pvals), c(padj))
+colnames(mat) <- c("rawP", "adjP")
+rownames(mat) <- names(pvals)
+
+test <- "t-test"
+normFacts <- "rank-simple"
+reject <-  names(padj)[which(padj < .05)]
+degenes <- degenes
+
+cor_rej <- sum(reject %in% degenes)
+err_rej <- sum(reject %!in% degenes)
+Specificity <- err_rej / ( nrow(otu_table(physeq)) -length(degenes) )
+Sensitivity <- cor_rej / length(degenes)
+FDR <- err_rej / (cor_rej + err_rej)
+
+res <- matrix(c( test,
+                 normFacts,
+                 Sensitivity,
+                 Specificity,
+                 FDR ), nrow = 1)
+colnames(res) <- c( "test",
+                    "norm",
+                    "sens",
+                    "spec",
+                    "fdr" )
+res[,"fdr"] <- ifelse(is.nan(res[,"fdr"]), 0, res[,"fdr"])
+res[,"fdr"] <- ifelse(res[,"fdr"] == "NaN", 0, res[,"fdr"])
+rawP <- pvals
+tTest_rank  <- list("res" = res,
+                    "pvals" = rawP)
+Sys.time() - start
+
+##############################################################
