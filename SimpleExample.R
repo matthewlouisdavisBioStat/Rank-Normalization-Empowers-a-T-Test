455f5c36d089a9fd7bb11a0dce7d88868f6f9a27
matthewlouisdavisBioStat <67028278+matthewlouisdavisBioStat@users.noreply.github.com>
2020-11-12 08:33:53 -0600

    Update SimpleExample.R

--- SimpleExample.R
+++ SimpleExample.R
@@ -1,7 +1,7 @@
 
-  # Simple demonstration of rank Normalization with a t-test using for loop
+  # Simple Demonstration of Rank Normalization Paired with a T-Test
 
-## Make up an OTU Table with Arbitrary Values
+## Make up an OTU Table with arbitrary Values
 otu_table <- matrix(rpois(100000,100)*rbinom(100000,1,0.5),nrow= 1000)
 colnames(otu_table) <- sample(c("control sample","treatment sample"),
                               ncol(otu_table),replace= TRUE)
@@ -10,10 +10,16 @@ treatment_indices <- which(colnames(otu_table) == "treatment sample")
 rownames(otu_table) <- paste0("OTU_",1:nrow(otu_table))
 head(otu_table[,1:5])
 
-## Perform DAA using Rank Normalization with a t-test
-otu_table <- otu_table[rowSums(otu_table) > 0,] #1) Exclude OTU with only 0s
-ranks <- apply(otu_table,2,rank) #2) Rank samples in ascending order
-results <- list() #3) Perform t-test across ranks with respect to the two groups
+## Perform DAA using rank normalization with a t-test
+#1) Exclude OTU with only 0s
+otu_table <- otu_table[rowSums(otu_table) > 0,] 
+
+#2) Rank samples in ascending order, using average-ties
+ranks <- apply(otu_table,2,rank) 
+
+#3) Perform t-test across ranks with respect to the two groups,
+#   for each OTU
+results <- list() 
 for(i in 1:nrow(ranks)){
   results[[paste0("OTU_",i)]] <- 
     t.test(ranks[i,control_indices],
