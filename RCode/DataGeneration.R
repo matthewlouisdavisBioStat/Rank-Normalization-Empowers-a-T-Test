90f0de11b93e6fe954b653f6d3beda0c66daf7a3
matthewlouisdavisBioStat <67028278+matthewlouisdavisBioStat@users.noreply.github.com>
2020-12-03 21:11:33 -0600

    Update DataGeneration.R

--- RCode/DataGeneration.R
+++ RCode/DataGeneration.R
@@ -4,8 +4,7 @@
 
 ## Contains only code necessary to generate simulated datasets considered
 ## Code was adapted for non-parallel computation and streamlined for H1 midV
-## Some Code is extraneous, artifacts from simulations not considered
-## I do not take credit for this code, this goes to Hawinkel et al.
+## from Hawinkel et al.
 
 
 ## distribution to generate data from
